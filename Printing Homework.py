ea070fbc022cfbec2c1111ff8109a9593ffe4470
VarunM25 <varunpmukund@gmail.com>
2022-01-20 19:47:14 -0600

    Printing Homework for 1/21/22

---
+++ Printing Homework.py
@@ -0,0 +1,27 @@
+import os
+os.system('cls')
+#Varun Mukund
+#1/19/22
+
+#Printing Assignment - 1/21/22
+
+
+#Question 1:
+print('Greenhill : 10,Portal : 5.33')
+print('Total students : 240, Boys : 120')
+print('031')
+print ('3.561E+02')
+
+#I am going to insert a space here so it is easier to read
+print('  ')
+
+#Question 2:
+print('Welcome to', end=' ')
+print('Greenhill School')
+
+#I am going to insert a space here so it is easier to read
+print('  ')
+
+#Question 3:
+print('I\nLove\t\"Greenhill\"')
+
