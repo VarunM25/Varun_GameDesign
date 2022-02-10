7b7778a9efb41b6d578837944df81d61639e8d14
VarunM25 <varunpmukund@gmail.com>
2022-02-10 09:37:47 -0600

    updated game

--- Word Guessing Game.py
+++ Word Guessing Game.py
@@ -12,13 +12,13 @@ def menu():
     ▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄░░▒▒▒▒▒
     ▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██▌░░▒▒▒▒
     ▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░▄▄███▀░░░░▒▒▒
-    ▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░█████░▄█░░░░▒▒  OWOOOOOOOOOOOO! You have entered the chamber of doom, where frustration meets word games...
-    ▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░▄████████▀░░░░▒▒  In this game, you will guess a word, that I CHOOSE, with NO HELP whatsoever
-    ▒▒░░░░░░░░░░░░░░░░░░░░░░░░▄█████████░░░░░░░▒  Before you start, pick a level! (Type the number near the level)
-    ▒░░░░░░░░░░░░░░░░░░░░░░░░░░▄███████▌░░░░░░░▒            1 - Basketball Players
-    ▒░░░░░░░░░░░░░░░░░░░░░░░░▄█████████░░░░░░░░▒            2 - Vegetables
-    ▒░░░░░░░░░░░░░░░░░░░░░▄███████████▌░░░░░░░░▒            3 - Computer Parts
-    ▒░░░░░░░░░░░░░░░▄▄▄▄██████████████▌░░░░░░░░▒
+    ▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░█████░▄█░░░░▒▒  OWOOOOOOOOOOOO! You have entered the chamber of doom, where frustration meets
+    ▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░▄████████▀░░░░▒▒  word games... In this game, you will guess a word, that I CHOOSE, with 
+    ▒▒░░░░░░░░░░░░░░░░░░░░░░░░▄█████████░░░░░░░▒  NO HELP whatsoever. Before you start, pick a level! 
+    ▒░░░░░░░░░░░░░░░░░░░░░░░░░░▄███████▌░░░░░░░▒  (Type the number near the level)          
+    ▒░░░░░░░░░░░░░░░░░░░░░░░░▄█████████░░░░░░░░▒            1 - Basketball Players
+    ▒░░░░░░░░░░░░░░░░░░░░░▄███████████▌░░░░░░░░▒            2 - Vegetables
+    ▒░░░░░░░░░░░░░░░▄▄▄▄██████████████▌░░░░░░░░▒            3 - Computer Parts
     ▒░░░░░░░░░░░▄▄███████████████████▌░░░░░░░░░▒
     ▒░░░░░░░░░▄██████████████████████▌░░░░░░░░░▒
     ▒░░░░░░░░████████████████████████░░░░░░░░░░▒
@@ -34,18 +34,15 @@ def menu():
     level = input('Pick a level from the three above...or else...(scroll up): ')
     if int(level) == 1:
         selectWord1()
-        guessFunction()
     elif int(level) ==2:
         selectWord2()
-        guessFunction()
     elif int(level) ==3:
         selectWord3()
-        guessFunction()
     # print (word)
 def selectWord1():
     global word
     os.system('cls')
-    Bball =["westbrook", 'lebron', 'durant', 'harden', 'kyrie', 'doncic', 'giannis', 'ja', 'curry','kobe', 'jordan']
+    Bball =["westbrook", 'lebron', 'durant', 'harden', 'kyrie', 'doncic', 'giannis', 'damian', 'curry','kobe', 'jordan']
     word = random.choice (Bball)
 def selectWord2():
     global word
@@ -73,23 +70,26 @@ def guessFunction():
 
 menu()
 
-
-
-GameOn = True
 def restart():
+    global GameOn
+    global tries
     playagain = input('Do you want to LOSE- I mean play again? Yes(1) or No(2)\n p.s. if you type anything, and I mean ANYTHING, else I am sending you back into the death maze - I mean game...')
     if int(playagain) == 2:
         os.system('cls')
         print ('You had', points, 'points')
         print ('Come back soon... to meet your doom')
+        print ('The high score is', Highscore)
         GameOn = False
     else:
+        tries = 0
         os.system('cls')
         print ('restarting...')
         time.sleep (2)
         menu()
+GameOn = True
 tries = 0
 points = 0
+Highscore = 0
 letterGuessed = ""
 while GameOn:
     guessFunction()
@@ -108,19 +108,27 @@ while GameOn:
         os.system ('cls')
         print ('\nHOW?! HOW DID YOU WIN...I will have my revenge')
         if tries < 2:
-            print ('You got 5 points...')
-            points += 5
+            points = len(word)*3-tries*2
             print ('You now have', points, 'points')
+            if points > Highscore:
+                print ('YOU GOT THE HIGH SCORE...I have severely underestimated you...')
+                Highscore += points
+                print ('The high score is', Highscore)
         elif tries >2 and tries <5:
-            print ('You got 2 points...')
-            points += 2
+            points = len(word)*3-tries*2
             print ('You now have', points, 'points')
+            if points > Highscore:
+                print ('YOU GOT THE HIGH SCORE...I have severely underestimated you...')
+                Highscore += points
+                print ('The high score is', Highscore)
         restart()
     elif tries >6:
         os.system('cls')
         print ('\nHA HA HA, you ran out of tries. Now let me call my brothers... we will feast on your remains... AWOOOOOOOOOO\n AND YOU GET NO POINTS HAHAHHAAH')
+        print ('The high score is', Highscore)
         tries = 0
         restart()
+
 
 
 
