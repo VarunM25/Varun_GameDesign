99d599a691ca1a591cc30a02b03d92ea11220f06
VarunM25 <varunpmukund@gmail.com>
2022-02-17 23:41:43 -0600

    more work needs to be done

--- cardGame.py
+++ cardGame.py
@@ -3,12 +3,14 @@
 import os
 os.system('cls')
 #Card War game
-numberCards=[]
-for i in range(2,11):
-    numberCards.append(i)
-numberCards.insert(0,'A')
+
 suits=['♠', '❤', '♣', '♦']
 royals=['J', 'Q', 'K']
+numberCards=[]
+for i in range(2,11):
+    numberCards.append(i) #putting numbers into the deck
+numberCards.insert(0,'A') #adding ace to the deck
+numberCards.extend(royals) #Adding royals now
 #Make a combined list [2,3,4,5,6,7,8,9,10,11,12,J,Q,K, etc...]
 #Make combinations for every card ex: 2c, 2h, 5s ...
 resultDeck=[]
@@ -19,16 +21,8 @@ def Deck(cardnumber):
     for i in range (len(suits)):
         suits[i]=str(numberCards[cardnumber])+str(suits[i])
     resultDeck.extend(suits)
-    suits=['♠', '❤', '♣', '♦']
+    suits=['♠', '❤', '♣', '♦'] #resetting suites
 for l in range(0,8):
     Deck(1)
     Deck (2)
-    Deck (3)
-    Deck (4)
-    Deck (5)
-    Deck (6)
-    Deck (7)
-    Deck (8)
-    Deck (9)
-
-print ('The Card Deck:\n\n'+str(resultDeck))
+print ('The Card Deck:\n\n'+str(resultDeck), '\n\n'+str(len(resultDeck)), 'cards\n')
