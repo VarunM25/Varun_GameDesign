5fae1c8bb8efa60bd7f749bca7155d8adb40a900
VarunM25 <varunpmukund@gmail.com>
2022-02-18 00:08:48 -0600

    even more stuff

--- cardGame.py
+++ cardGame.py
@@ -2,9 +2,8 @@
 #2/16/22
 import os
 os.system('cls')
-#Card War game
-
-suits=['♠', '❤', '♣', '♦']
+#Card Game
+suits=['♠', '♦', '♣', '♥']
 royals=['J', 'Q', 'K']
 numberCards=[]
 for i in range(2,11):
@@ -14,7 +13,7 @@ numberCards.extend(royals) #Adding royals now
 #Make a combined list [2,3,4,5,6,7,8,9,10,11,12,J,Q,K, etc...]
 #Make combinations for every card ex: 2c, 2h, 5s ...
 resultDeck=[]
-def Deck(cardnumber):
+def Deck(cardnumber): #adds suits to the numbers - makes parameters for diff cards
     global suits
     global numberCards
     global resultDeck
@@ -22,7 +21,6 @@ def Deck(cardnumber):
         suits[i]=str(numberCards[cardnumber])+str(suits[i])
     resultDeck.extend(suits)
     suits=['♠', '❤', '♣', '♦'] #resetting suites
-for l in range(0,8):
+for l in range(0,13):
     Deck(1)
-    Deck (2)
 print ('The Card Deck:\n\n'+str(resultDeck), '\n\n'+str(len(resultDeck)), 'cards\n')
