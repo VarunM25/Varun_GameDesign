15ac93f218fd59ee593967aaf461bf90d31f5914
VarunM25 <varunpmukund@gmail.com>
2022-03-21 09:54:31 -0500

    ok

--- learningfont.py
+++ learningfont.py
@@ -14,6 +14,7 @@ INSTRUCTIONS_FNT = pygame.font.SysFont('comicsans', 25)
 text= TITLE_FNT.render('Hello', 1, (255,0,0))
 wind.fill((255,255,255))
 wind.blit(text,(50,50))
+text= INSTRUCTIONS_FNT.render('Write your instructions', 1, (0,255,0))
+wind.blit(text,(220,220))
 pygame.display.update()
-
 pygame.time.delay(10000)
