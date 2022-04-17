d4a216995132a9160d90364850478d9d070eb68b
VarunM25 <varunpmukund@gmail.com>
2022-04-17 18:25:13 -0500

    LOTS of new characters

--- Class Stuff/images/Pygame-Tutorials-master/Game/Final Game/Final_v.1.py
+++ Class Stuff/images/Pygame-Tutorials-master/Game/Final Game/Final_v.1.py
@@ -13,3 +13,19 @@ bg2 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkg
 bg3 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd3.jpg'), (500,480))
 bg4 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd4.jpg'), (500,480))
 bg5 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd5.jpg'), (500,480))
+
+#Characters
+
+char = pygame.image.load('Class Stuff\images\Idle-stance_04.png')
+attacking = [pygame.image.load('Class Stuff\images\Attacking_01.png'), pygame.image.load('Class Stuff\images\Attacking_02.png'), pygame.image.load('Class Stuff\images\Attacking_03.png'), pygame.image.load('Class Stuff\images\Attacking_04.png'), pygame.image.load('Class Stuff\images\Attacking_05.png'), pygame.image.load('Class Stuff\images\Attacking_06.png'), pygame.image.load('Class Stuff\images\Attacking_07.png')]
+running = [pygame.image.load('Class Stuff\images\Running_01.png'), pygame.image.load('Class Stuff\images\Running_02.png'), pygame.image.load('Class Stuff\images\Running_03.png'), pygame.image.load('Class Stuff\images\Running_04.png'), pygame.image.load('Class Stuff\images\Running_05.png'), pygame.image.load('Class Stuff\images\Running_06.png'), pygame.image.load('Class Stuff\images\Running_07.png'), pygame.image.load('Class Stuff\images\Running_08.png'), pygame.image.load('Class Stuff\images\Running_09.png'), pygame.image.load('Class Stuff\images\Running2_01.png'), pygame.image.load('Class Stuff\images\Running2_02.png'), pygame.image.load('Class Stuff\images\Running2_03.png')]
+jumping = [pygame.image.load('Class Stuff\images\Jumping_01.png'), pygame.image.load('Class Stuff\images\Jumping_02.png'), pygame.image.load('Class Stuff\images\Jumping_03.png')]
+crouching = [pygame.image.load('Class Stuff\images\crouching_01.png'), pygame.image.load('Class Stuff\images\crouching_02.png'), pygame.image.load('Class Stuff\images\crouching_03.png')]
+
+
+
+Area1 = True
+Area2 = False
+Area3 = False
+Area4 = False
+Area5 = False
