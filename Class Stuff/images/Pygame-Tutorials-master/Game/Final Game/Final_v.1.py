6146a7545cd7a62b670f6e1077e913407cd56149
VarunM25 <varunpmukund@gmail.com>
2022-04-21 08:44:11 -0500

    stuff

--- Class Stuff/images/Pygame-Tutorials-master/Game/Final Game/Final_v.1.py
+++ Class Stuff/images/Pygame-Tutorials-master/Game/Final Game/Final_v.1.py
@@ -4,15 +4,15 @@ import os, pygame
 
 os.system('cls')
 pygame.init()
-win=pygame.display.set_mode((500,480))
+win=pygame.display.set_mode((700,600))
 pygame.display.set_caption('Final Game')
 
 #background variables
-bg1 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd1.jpg'), (500,480))
-bg2 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd2.jpg'), (500,480))
-bg3 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd3.jpg'), (500,480))
-bg4 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd4.jpg'), (500,480))
-bg5 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd5.jpg'), (500,480))
+bg1 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd1.jpg'), (700,600))
+bg2 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd2.jpg'), (700,600))
+bg3 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd3.jpg'), (700,600))
+bg4 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd4.jpg'), (700,600))
+bg5 = pygame.transform.scale(pygame.image.load('Class Stuff\images\\finalgamebkgd5.jpg'), (700,600))
 
 #Characters
 
